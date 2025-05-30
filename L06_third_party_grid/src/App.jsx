d0f2eb3946556e1e2ba94cf029c0737cc8b12614
lohikrme <chinese.parrotlet@gmail.com>
2025-05-30 12:25:25 +0300

    attempting get ag-grid work

---
+++ L06_third_party_grid/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+
+function App() {
+  const [todo, setTodo] = useState({description: '', date: '', status: ''});
+  const [todos, setTodos] = useState([]);
+
+  // Column definitions for ag-grid
+  const columnDefs = [
+    { field: 'description', filter: true },
+    { field: 'date', filter: true },
+    { field: 'status', filter: true }
+  ]
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({description: '', date: '', status: ''});
+  }
+
+  return (
+    <>
+      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      <input placeholder="Status" name="status" value={todo.status} onChange={inputChanged}/>
+      <button onClick={addTodo}>Add</button>
+      <div className="ag-theme-material" style={{height: 400, width: 600}}>
+         <AgGridReact
+            rowData={todos}
+            columnDefs={columnDefs}
+         />
+      </div>
+    </>
+  );
+}
+
+export default App;
