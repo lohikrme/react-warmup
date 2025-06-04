587a2b149201ece516c9f4939bf2dad7faffa638
lohikrme <chinese.parrotlet@gmail.com>
2025-06-04 11:09:31 +0300

    ag-grid deleting and multirow selection works now!

--- L06_router/src/components/Missions.jsx
+++ L06_router/src/components/Missions.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import './styles/Missions.css'
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
 import { AgGridReact } from 'ag-grid-react'
+import { useMemo } from 'react';
+import { useRef } from 'react';
+
+
 
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
+
 
 const startData = [
     {description: "Kill Yoda", status: "pending"}, 
@@ -16,31 +21,41 @@ const startData = [
 function Missions() {
   const [tasks, setTasks] = useState(startData)
   const [newTask, setNewTask] = useState()
-  const [newStatus, setNewStatus] = useState()
+  const [newStatus, setNewStatus] = useState("pending")
+  const gridRef = useRef();
 
   const [colDefs, setColDefs] = useState([
-    {headerCheckboxSelection: true, checkboxSelection: true, width: 50},
     {field: "description", minWidth: 400},
     {field: "status", filter: true}
   ])
 
-
+  const rowSelection = useMemo(() => { 
+    return {
+          mode: 'multiRow',
+          checkboxes: (params) => {
+          console.log({params})
+          return true
+          },
+      };
+  }, []);
 
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
       setTasks([...tasks, {description: newTask, status: newStatus}])
       setNewTask('')
-      setNewStatus('')
+      setNewStatus("pending")
     }
   };
 
   const handleDeleteSelected = () => {
     console.log("attempt to delete")
-    /*
-    const selectedNodes = gridRef.current.api.getSelectedNodes()
-    console.log(selectedNodes)
-    return
-    */
+    let selectedNodes = gridRef.current?.api.getSelectedRows()
+    let selectedDescriptions = selectedNodes.map(node => node.description)
+    console.log(selectedDescriptions)
+    let updatedTasks = tasks.filter((task) => !selectedDescriptions.includes(task.description))
+    console.log(tasks)
+    console.log(updatedTasks)
+    setTasks(updatedTasks)
   }
 
   const handleTaskChange = (event) => {
@@ -76,9 +91,15 @@ function Missions() {
       </main>
       <div id="ag_grid" style={{ height: 500, width: 1000 }}>
         <AgGridReact
+          ref={gridRef}
           rowData={tasks}
           columnDefs={colDefs}
-          rowSelection="multiple"
+          rowSelection={rowSelection}
+          onSelectionChanged={(event) => {
+                if (event.selectedNodes.length > 0) {
+                      event.selectedNodes.map(node => console.log(node.data)) }
+                else console.log("no selectedNodes")
+          }}
         />
       </div>
     </div>
